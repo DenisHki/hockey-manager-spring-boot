292b7f7d2605a7333e1cb3accb5df7abc165cd3b
Denis Chuvakov <ds.chuvakov@gmail.com>
2023-10-26 10:10:37 +0300

    Started the project

---
+++ src/main/java/com/chuvakov/hockeymanager/HockeymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.chuvakov.hockeymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HockeymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chuvakov/hockeymanager/HockeymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuvakov.hockeymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
